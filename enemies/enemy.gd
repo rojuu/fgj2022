8856b9de40bed8884ef6144779784ad96184189c
Roni Juppi <roju269@gmail.com>
2022-01-29 18:30:57 +0200

    Fix enemy getting stuck

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,8 +2,9 @@ extends BaseEnemy
 
 export var speed = 10
 export var flying = false
-export var gravity = 100
+export var gravity = 9.8
 var velocity = Vector3.ZERO
+var gravity_velocity = 0
 var player
 var player_position
 var forward
@@ -19,7 +20,8 @@ func _physics_process(delta):
 		forward.y = 0
 	velocity = forward * speed
 	if (!flying):
-			velocity.y -= gravity
+			gravity_velocity -= gravity
+	velocity.y += gravity_velocity
 	move_and_slide(velocity, Vector3(0,1,0))
 	if is_on_floor():
-		velocity.y = 0
+		gravity_velocity = 0
